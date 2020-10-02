907651c7abbc47883af3e6fe6cf38db505dcf4fa
defrindr <defrindr@gmail.com>
2020-10-02 07:48:02 +0700

    add handler

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +14,44 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+if (Schema::hasTable('routes') && Schema::hasTable('route_groups')) {
+    try {
+        $routes = DB::table('routes')
+            ->join('route_groups', 'routes.route_group_id', 'route_groups.id')
+            ->select(
+                DB::raw("concat(route_groups.name, '.', routes.name) as full_name,concat(route_groups.url, '/', routes.url) as full_url, route_groups.*, routes.*"),
+            )->where(['route_groups.type' => 'api'])->get();
+
+        if (count($routes) > 0) {
+
+            foreach ($routes as $route) {
+                $middleware = [];
+                if ($route->middleware != null) {
+                    $middleware = explode('|', "" . $route->middleware);
+                }
+                if (count($middleware) > 0) {
+                    $method = $routes->method;
+                    Route::middleware($middleware)->$method($route->full_url, [$route->class, $route->function])
+                        ->name($route->full_name);
+                } else {
+                    $method = $route->method;
+                    Route::$method($route->full_url, [$route->class, $route->function])
+                        ->name($route->full_name);
+                }
+            }
+        }
+    } catch (\Throwable $e) {
+        printf($e);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +13,39 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+if (Schema::hasTable('routes') && Schema::hasTable('route_groups')) {
+    try {
+        $routes = DB::table('routes')
+            ->join('route_groups', 'routes.route_group_id', 'route_groups.id')
+            ->select(
+                DB::raw("concat(route_groups.name, '.', routes.name) as full_name,concat(route_groups.url, '/', routes.url) as full_url, route_groups.*, routes.*"),
+            )->where(['route_groups.type' => 'web'])->get();
+
+        if (count($routes) > 0) {
+
+            foreach ($routes as $route) {
+                $middleware = [];
+                if ($route->middleware != null) {
+                    $middleware = explode('|', "" . $route->middleware);
+                }
+                if (count($middleware) > 0) {
+                    $method = $routes->method;
+                    Route::middleware($middleware)->$method($route->full_url, [$route->class, $route->function])
+                        ->name($route->full_name);
+                } else {
+                    $method = $route->method;
+                    Route::$method($route->full_url, [$route->class, $route->function])
+                        ->name($route->full_name);
+                }
+            }
+        }
+    } catch (\Throwable $e) {
+        printf($e);
+    }
+}
